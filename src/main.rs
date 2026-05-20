b7ef059cbe8dd5fd5fbd72c6c251d974171dcc5b
Enderic <133310359+Noodles-12@users.noreply.github.com>
2026-05-19 22:36:21 -0500

    init commit

---
+++ src/main.rs
@@ -0,0 +1,25 @@
+#![no_std]
+#![no_main]
+
+use embassy_executor::Spawner;
+use embassy_stm32::gpio::{Level, Output, Speed};
+use embassy_time::Timer;
+use defmt::info;
+use {defmt_rtt as _, panic_probe as _};
+
+#[embassy_executor::main]
+async fn main(_spawner: Spawner) {
+    let p = embassy_stm32::init(Default::default());
+
+    let mut led = Output::new(p.PA5, Level::Low, Speed::Low);
+
+    loop {
+        info!("on");
+        led.set_high();
+        Timer::after_millis(500).await;
+
+        info!("off");
+        led.set_low();
+        Timer::after_millis(500).await;
+    }
+}
